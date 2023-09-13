22e919d40e7d4ba93745f4aaadf283a36d96069c
ZubriQ <zubrich157@gmail.com>
2023-09-13 20:42:54 +0700

    feat: halstead output values class

---
+++ 1-Halstead-Metrics/HalsteadResult.cs
@@ -0,0 +1,8 @@
+namespace _1_Halstead_Metrics;
+
+public class HalsteadResult
+{
+    public double Dispersion { get; set; }
+    public double ExpectedValue { get; set; }
+    public double NewExpectedSampleSize { get; set; }
+}
