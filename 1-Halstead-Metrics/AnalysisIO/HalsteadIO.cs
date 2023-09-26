2f7cf37ff14d14c58cbf45814f98022c6ab0ad65
ZubriQ <zubrich157@gmail.com>
2023-09-27 02:10:15 +0700

    style: output

--- 1-Halstead-Metrics/AnalysisIO/HalsteadIO.cs
+++ 1-Halstead-Metrics/AnalysisIO/HalsteadIO.cs
@@ -6,9 +6,9 @@ public static class HalsteadIO
 {
     public static (int, int) InputParameters()
     {
-        Console.WriteLine("Enter dictionary size:");
+        Console.WriteLine("Enter dictionary size (for example, 16):");
         var dictionarySize = int.Parse(Console.ReadLine()!);
-        Console.WriteLine("Enter sample size:");
+        Console.WriteLine("Enter sample size (for example, 100):");
         var sampleSize = int.Parse(Console.ReadLine()!);
 
         return (dictionarySize, sampleSize);
@@ -19,19 +19,21 @@ public static class HalsteadIO
         Console.WriteLine();
         
         Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.WriteLine($"Average (M):\t\t {Math.Round(result.StatisticalValues.ExpectedValue, 2)}");
-        Console.WriteLine($"Disperse (D):\t\t {Math.Round(result.StatisticalValues.Dispersion, 2)}");
-        Console.WriteLine($"Measurement error (d):\t {result.StatisticalValues.MeasurementError}");
+        Console.WriteLine("Statistical Values:");
+        Console.WriteLine($"Average (M):\t\t{Math.Round(result.StatisticalValues.ExpectedValue, 2)}");
+        Console.WriteLine($"Disperse (D):\t\t{Math.Round(result.StatisticalValues.Dispersion, 2)}");
+        Console.WriteLine($"Measurement error (d):\t{result.StatisticalValues.MeasurementError}");
         Console.ResetColor();
         
         Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"Average (M):\t\t {Math.Round(result.TheoreticalValues.ExpectedValue, 2)}");
-        Console.WriteLine($"Disperse (D):\t\t {Math.Round(result.TheoreticalValues.Dispersion, 2)}");
-        Console.WriteLine($"Measurement error (d):\t {result.TheoreticalValues.MeasurementError}");
+        Console.WriteLine("Theoretical Values:");
+        Console.WriteLine($"Average (M):\t\t{Math.Round(result.TheoreticalValues.ExpectedValue, 2)}");
+        Console.WriteLine($"Disperse (D):\t\t{Math.Round(result.TheoreticalValues.Dispersion, 2)}");
+        Console.WriteLine($"Measurement error (d):\t{result.TheoreticalValues.MeasurementError}");
         Console.ResetColor();
 
         Console.ForegroundColor = ConsoleColor.Magenta;
-        Console.WriteLine($"New sample size (N):\t{(int)result.NewExpectedSampleSize:### ### ##0}");
+        Console.WriteLine($"Stable Sample Size:\t{(int)result.NewExpectedSampleSize:### ### ##0}");
         Console.ResetColor();
 
         Console.WriteLine();
