f8117a1da476280678e195588a939e03f3723059
ZubriQ <zubrich157@gmail.com>
2023-10-02 02:46:37 +0700

    refactor: more clear method names

--- 2-Triangle-Type-Detector.Tests.NUnit/TriangleTypeDetectorTests.cs
+++ 2-Triangle-Type-Detector.Tests.NUnit/TriangleTypeDetectorTests.cs
@@ -4,7 +4,7 @@ namespace _2_Triangle_Type_Detector.Tests.NUnit;
 
 public class TriangleTypeDetectorTests
 {
-    private static void TestTriangle(double sideA, double sideB, double sideC, TriangleType expected)
+    private static void AssertTriangleType(double sideA, double sideB, double sideC, TriangleType expected)
     {
         Assert.That(TriangleTypeDetector.DetermineType(new Triangle(sideA, sideB, sideC)), Is.EqualTo(expected));
     }
@@ -13,18 +13,18 @@ public class TriangleTypeDetectorTests
     [TestCase(3, 4, 5, TriangleType.RightAngled)]
     [TestCase(50, 50, 1, TriangleType.Isosceles)]
     [TestCase(2, 2, 55, TriangleType.Invalid)]
-    public void TestEquivalencePartitioning(double sideA, double sideB, double sideC, TriangleType expected)
+    public void TestTriangleTypeWithEquivalencePartitioning(double sideA, double sideB, double sideC, TriangleType expected)
     {
-        TestTriangle(sideA, sideB, sideC, expected);
+        AssertTriangleType(sideA, sideB, sideC, expected);
     }
 
     [TestCase(double.MaxValue, double.MaxValue, double.MaxValue, TriangleType.Equilateral)]
     [TestCase(double.MaxValue * 2, double.MaxValue * 2, double.MaxValue * 2, TriangleType.Invalid)]
     [TestCase(double.MinValue, double.MinValue, double.MinValue, TriangleType.Invalid)]
     [TestCase(0, 0, 0, TriangleType.Invalid)]
-    public void TestBoundaryValues(double sideA, double sideB, double sideC, TriangleType expected)
+    public void TestTriangleTypeWithBoundaryValues(double sideA, double sideB, double sideC, TriangleType expected)
     {
-        TestTriangle(sideA, sideB, sideC, expected);
+        AssertTriangleType(sideA, sideB, sideC, expected);
     }
     
     [TestCase(0.5, 1, 0.5, TriangleType.Invalid)]
@@ -32,8 +32,8 @@ public class TriangleTypeDetectorTests
     [TestCase(0, 0, 0, TriangleType.Invalid)]
     [TestCase(-2, -5, -3, TriangleType.Invalid)]
     [TestCase(3, 4, 5, TriangleType.RightAngled)]
-    public void TestCausalAnalysis(double sideA, double sideB, double sideC, TriangleType expected)
+    public void TestTriangleTypeWithCausalAnalysis(double sideA, double sideB, double sideC, TriangleType expected)
     {
-        TestTriangle(sideA, sideB, sideC, expected);
+        AssertTriangleType(sideA, sideB, sideC, expected);
     }
 }
