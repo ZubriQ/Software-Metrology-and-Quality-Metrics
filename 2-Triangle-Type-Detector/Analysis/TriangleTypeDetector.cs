be3ddd67d6e1e1445023ef86b6fc06a0e54785a2
ZubriQ <zubrich157@gmail.com>
2023-10-01 22:02:49 +0700

    refactor: methods' return types

--- 2-Triangle-Type-Detector/Analysis/TriangleTypeDetector.cs
+++ 2-Triangle-Type-Detector/Analysis/TriangleTypeDetector.cs
@@ -4,54 +4,59 @@ public static class TriangleTypeDetector
 {
     private const double Tolerance = 1e-10;
 
-    public static TriangleType DetermineType(Triangle triangle)
+    public static TriangleType? DetermineType(Triangle triangle)
     {
-        if (IsValid(triangle))
+        return IsValid(triangle) ?? IsEquilateral(triangle) ?? IsRight(triangle) ?? IsIsosceles(triangle);
+    }
+    
+    private static TriangleType? IsValid(Triangle triangle)
+    {
+        if (!(triangle.SideA + triangle.SideB > triangle.SideC &&
+              triangle.SideB + triangle.SideC > triangle.SideA &&
+              triangle.SideA + triangle.SideC > triangle.SideB))
         {
             return TriangleType.Invalid;
         }
-        
-        if (IsEquilateral(triangle))
+
+        return null;
+    }
+    
+    private static TriangleType? IsEquilateral(Triangle triangle)
+    {
+        if (Math.Abs(triangle.SideA - triangle.SideB) < Tolerance &&
+            Math.Abs(triangle.SideB - triangle.SideC) < Tolerance)
         {
             return TriangleType.Equilateral;
         }
-        
-        if (IsRight(triangle))
-        {
-            return TriangleType.Right;
-        }
 
-        return IsIsosceles(triangle) ? TriangleType.Isosceles : TriangleType.Scalene;
+        return null;
     }
-
-    private static bool IsIsosceles(Triangle triangle)
-    {
-        return Math.Abs(triangle.SideA - triangle.SideB) < Tolerance ||
-               Math.Abs(triangle.SideA - triangle.SideC) < Tolerance ||
-               Math.Abs(triangle.SideB - triangle.SideC) < Tolerance;
-    }
-
-    private static bool IsRight(Triangle triangle)
+    
+    private static TriangleType? IsRight(Triangle triangle)
     {
         var aSquared = triangle.SideA * triangle.SideA;
         var bSquared = triangle.SideB * triangle.SideB;
         var cSquared = triangle.SideC * triangle.SideC;
-        
-        return Math.Abs(aSquared - (bSquared + cSquared)) < Tolerance ||
-               Math.Abs(bSquared - (aSquared + cSquared)) < Tolerance ||
-               Math.Abs(cSquared - (bSquared + aSquared)) < Tolerance;
+
+        if (Math.Abs(aSquared - (bSquared + cSquared)) < Tolerance ||
+            Math.Abs(bSquared - (aSquared + cSquared)) < Tolerance ||
+            Math.Abs(cSquared - (bSquared + aSquared)) < Tolerance)
+        {
+            return TriangleType.Right;
+        }
+
+        return null;
     }
 
-    private static bool IsEquilateral(Triangle triangle)
+    private static TriangleType? IsIsosceles(Triangle triangle)
     {
-        return Math.Abs(triangle.SideA - triangle.SideB) < Tolerance &&
-               Math.Abs(triangle.SideB - triangle.SideC) < Tolerance;
-    }
+        if (Math.Abs(triangle.SideA - triangle.SideB) < Tolerance ||
+            Math.Abs(triangle.SideA - triangle.SideC) < Tolerance ||
+            Math.Abs(triangle.SideB - triangle.SideC) < Tolerance)
+        {
+            return TriangleType.Isosceles;
+        }
 
-    private static bool IsValid(Triangle triangle)
-    {
-        return triangle.SideA + triangle.SideB > triangle.SideC &&
-               triangle.SideB + triangle.SideC > triangle.SideA &&
-               triangle.SideA + triangle.SideC > triangle.SideB;
+        return TriangleType.Scalene;
     }
 }
