bf36bb9a4db0a62886e0ce8005e0d9761f60b154
ZubriQ <zubrich157@gmail.com>
2023-10-01 22:28:52 +0700

    feat: NUnit project

---
+++ 2-Triangle-Type-Detector.Tests.NUnit/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace _2_Triangle_Type_Detector.Tests.NUnit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
