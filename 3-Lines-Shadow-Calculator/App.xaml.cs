6bb7f48a784042e5f57a2af52b0e89224c22310f
ZubriQ <zubrich157@gmail.com>
2023-10-12 04:05:32 +0700

    feat: viewing lines data in a listbox

--- 3-Lines-Shadow-Calculator/App.xaml.cs
+++ 3-Lines-Shadow-Calculator/App.xaml.cs
@@ -6,12 +6,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace _3_Lines_Shadow_Calculator
+namespace _3_Lines_Shadow_Calculator;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
 }
--- 3-Lines-Shadow-Calculator/MainWindow.xaml.cs
+++ 3-Lines-Shadow-Calculator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,39 +15,52 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using _3_Lines_Shadow_Calculator.LineLibrary;
 
-namespace _3_Lines_Shadow_Calculator
+namespace _3_Lines_Shadow_Calculator;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private ObservableCollection<Line> CanvasLines { get; }
+    private Models.Point StartPoint => new(TextBoxLineStartX.Text, TextBoxLineStartY.Text);
+    private Models.Point EndPoint => new(TextBoxLineEndX.Text, TextBoxLineEndY.Text);
+        
+    public MainWindow()
     {
-        private Models.Point EndPoint => new(TextBoxLineEndX.Text, TextBoxLineEndY.Text);
-        private Models.Point StartPoint => new(TextBoxLineStartX.Text, TextBoxLineStartY.Text);
+        InitializeComponent();
+        CanvasLines = new ObservableCollection<Line>();
+        ListBoxLines.ItemsSource = CanvasLines;
 
-        public MainWindow()
+        //var lines = Canvas.Children.OfType<Line>().Where(l => l.Name != "LineAbscissa").ToArray();
+    }
+    
+    private void ButtonAddLine_OnClick(object sender, RoutedEventArgs e)
+    {
+        try
         {
-            InitializeComponent();
-            //var lines = Canvas.Children.OfType<Line>().Where(l => l.Name != "LineAbscissa").ToArray();
+            AddLineToCanvas();
+            ResetInput();
         }
-
-        private void ButtonAddLine_OnClick(object sender, RoutedEventArgs e)
+        catch (FormatException)
         {
-            try
-            {
-                AddLineToCanvas();
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Please enter valid numbers for the line coordinates.", "Invalid Input",
-                    MessageBoxButton.OK, MessageBoxImage.Error);
-            }
+            MessageBox.Show("Please enter valid numbers for the line coordinates.", "Invalid Input",
+                MessageBoxButton.OK, MessageBoxImage.Error);
         }
-
-        private void AddLineToCanvas()
-        {
-            var line = LineCreator.CreateLine(StartPoint, EndPoint);
-            Canvas.Children.Add(line);
-        }
+    }
+    
+    private void AddLineToCanvas()
+    {
+        var line = LineCreator.CreateLine(StartPoint, EndPoint);
+        CanvasLines.Add(line);
+        Canvas.Children.Add(line);
+    }
+    
+    private void ResetInput()
+    {
+        TextBoxLineStartX.Text = string.Empty;
+        TextBoxLineStartY.Text = string.Empty;
+        TextBoxLineEndX.Text = string.Empty;
+        TextBoxLineEndY.Text = string.Empty;
     }
 }
