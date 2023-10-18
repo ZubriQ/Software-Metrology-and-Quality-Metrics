214d4220de8fb8a1f32d32e9ae836aea61a220d0
ZubriQ <zubrich157@gmail.com>
2023-10-18 20:23:17 +0700

    feat: converting an infix expression into a postfix expression

--- 4-Reverse-Polish-Notation/Program.cs
+++ 4-Reverse-Polish-Notation/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace _4_Reverse_Polish_Notation;
+﻿using _4_Reverse_Polish_Notation.Library;
+
+namespace _4_Reverse_Polish_Notation;
 
 internal class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        while (true)
+        {
+            string infixExpression = "3 + 4 * (2 - 1)";
+            string postfixExpression = infixExpression.ToPostfix();
+            if (postfixExpression != null)
+            {
+                Console.WriteLine("Postfix: " + postfixExpression);
+            }
+
+            Console.ReadLine();
+        }
     }
 }
