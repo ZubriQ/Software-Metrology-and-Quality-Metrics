1fc308cc0868fae0a34cba2f24b0e5aa324d6d47
ZubriQ <zubrich157@gmail.com>
2023-10-28 22:30:20 +0700

    feat: statement coverage tests

---
+++ 4-Reverse-Polish-Notation.Tests.NUnit/White-box/StatementCoverageTests.cs
@@ -0,0 +1,11 @@
+﻿namespace _4_Reverse_Polish_Notation.Tests.NUnit.White_box;
+
+internal class StatementCoverageTests
+{
+    [TestCase("3 * (5 - 1) / (4 + 4)", "351-*44+/")]
+    [TestCase("(5 + (6 / 2)) - (2 * 8)", "562/+28*-")]
+    public void When_ValidInput(string infixExpression, string expectedPostfixExpression)
+    {
+        Tester.TestToPostfixExpression(infixExpression, expectedPostfixExpression);
+    }
+}
