ef2c6f51afe3923249c2e82c7b7f567975099b69
ZubriQ <zubrich157@gmail.com>
2023-10-28 22:05:21 +0700

    feat: error suggestion test

---
+++ 4-Reverse-Polish-Notation.Tests.NUnit/Black-box/ErrorSuggestionTests.cs
@@ -0,0 +1,12 @@
+﻿namespace _4_Reverse_Polish_Notation.Tests.NUnit.Black_box;
+
+[TestFixture]
+internal class ErrorSuggestionTests
+{
+    // Should generate a failed test.
+    [TestCase("1 - [5 + 3]", "incorrect data")]
+    public void When_InvalidInput(string infixExpression, string expectedPostfixExpression)
+    {
+        Tester.TestToPostfixExpression(infixExpression, expectedPostfixExpression);
+    }
+}
