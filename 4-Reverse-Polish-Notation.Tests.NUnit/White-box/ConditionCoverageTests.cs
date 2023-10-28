49bd98c60fce7d8161026584a43f8ace1f2852d0
ZubriQ <zubrich157@gmail.com>
2023-10-28 23:21:28 +0700

    feat: condition coverage tests

---
+++ 4-Reverse-Polish-Notation.Tests.NUnit/White-box/ConditionCoverageTests.cs
@@ -0,0 +1,16 @@
+﻿namespace _4_Reverse_Polish_Notation.Tests.NUnit.White_box;
+
+internal class ConditionCoverageTests
+{
+    [TestCase("5 + (6 / 3 * 2) - 7", "563/2*+7-")]
+    public void When_ValidInput(string infixExpression, string expectedPostfixExpression)
+    {
+        Tester.TestToPostfixExpression(infixExpression, expectedPostfixExpression);
+    }
+
+    [TestCase("33 + (40 / 20)", "incorrect data")]
+    public void When_InvalidInput(string infixExpression, string expectedPostfixExpression)
+    {
+        Tester.TestToPostfixExpression(infixExpression, expectedPostfixExpression);
+    }
+}
