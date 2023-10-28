4c76ebf94554d7982560cf5af8795f9bd2d29711
ZubriQ <zubrich157@gmail.com>
2023-10-28 23:50:31 +0700

    fix: decision criterion tests

--- 4-Reverse-Polish-Notation.Tests.NUnit/White-box/DecisionCriterionTests.cs
+++ 4-Reverse-Polish-Notation.Tests.NUnit/White-box/DecisionCriterionTests.cs
@@ -1,17 +1,20 @@
-﻿//namespace _4_Reverse_Polish_Notation.Tests.NUnit.White_box;
+﻿namespace _4_Reverse_Polish_Notation.Tests.NUnit.White_box;
 
-//[TestFixture]
-//internal class DecisionCriterionTests
-//{
-//    [TestCase("5 + (6 / 3 * 2) - 7", "")]
-//    public void When_ValidInput(string infixExpression, string expectedPostfixExpression)
-//    {
-//        Tester.TestToPostfixExpression(infixExpression, expectedPostfixExpression);
-//    }
+[TestFixture]
+internal class DecisionCriterionTests
+{
+    [TestCase("(5 + 5) / 5 * 1 - 5", "55+5/1*5-")]
+    [TestCase("5 + 0", "50+")]
+    public void When_ValidInput(string infixExpression, string expectedPostfixExpression)
+    {
+        Tester.TestToPostfixExpression(infixExpression, expectedPostfixExpression);
+    }
 
-//    [TestCase("33 + (40 / 20)", "incorrect data")]
-//    public void When_InvalidInput(string infixExpression, string expectedPostfixExpression)
-//    {
-//        Tester.TestToPostfixExpression(infixExpression, expectedPostfixExpression);
-//    }
-//}
+    [TestCase("33 + (40 / 20)", "incorrect data")]
+    [TestCase("a + (b / c)", "incorrect data")]
+    [TestCase("{2 + 3}", "incorrect data")] // Should fail the test with {} characters
+    public void When_InvalidInput(string infixExpression, string expectedPostfixExpression)
+    {
+        Tester.TestToPostfixExpression(infixExpression, expectedPostfixExpression);
+    }
+}
